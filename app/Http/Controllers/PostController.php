80fdbd9b2e09667451d18aadcefd7bf72d4df32f
ShoichiroKanai <goldwell0427@gmail.com>
2022-10-10 18:48:30 +0900

    add post_create error sistem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,38 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
-/**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]); 
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
-    
+
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
-     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
-    
+
     public function create()
     {
         return view('posts/create');
     }
-    
-    public function store(Request $request, Post $post)
+
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
-    
 }
